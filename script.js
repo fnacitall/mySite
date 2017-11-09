97d876deff0d698cff66496d1b31effee9215437
Ekaterina Myznikova <harlahtaa@gmail.com>
2017-11-09 19:39:39 +0700

    Добавит обработчик нажатия кнопки id=btn

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+	var btn = document.querySelector('btn');
+
+	btn.addEventListener('click', function (ev) {
+		alert('HELLO');
+	})
+})
